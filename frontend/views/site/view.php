a8ace58834f1c4fa66cb05e4774aff0d94933285
DostonOlimov <dostonolimov61@gmail.com>
2022-11-15 11:06:12 +0500

    before put on the server

--- frontend/views/site/view.php
+++ frontend/views/site/view.php
@@ -1,6 +1,8 @@
 <?php
 use yii\helpers\Url;
 use yii\helpers\Html;
+use common\models\Product;
+use common\models\RiskType;
 
 $this->title = Yii::$app->name;
 $lang = Yii::$app->language;
@@ -50,6 +52,16 @@ $lang = Yii::$app->language;
 							</div>
 						</div>
 					</div>
+					<div style="background-color: #FFFFFF; padding: 10px; padding-bottom: 0px; margin-bottom: 30px;">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <a class="btn btn-primary btn-block btn-top" href="<?= Url::to(['/site/index']) ?>"><?= Yii::t('app', 'Импортные продукты') ?></a>
+                            </div>
+                            <div class="col-lg-6">
+                                <a class="btn btn-primary btn-block btn-top" href="<?= Url::to(['/site/indexuz']) ?>"><?= Yii::t('app', 'Продукция производства Узбекистана') ?></a>
+                            </div>
+                        </div>
+                    </div>
 				</div>
 			</div>
 			<div class="row">
@@ -69,9 +81,18 @@ $lang = Yii::$app->language;
 								<span class="glyphicon glyphicon-eye-open" aria-hidden="true" style="font-size: 80%"></span> <?= $model->views ?>
 							</div>
 						</div>
+						
 						<div style="margin-top: 10px;">
 							<b><?= $model->getAttributeLabel('category') ?>:</b> <?= (isset($model->categoryName)) ? $model->categoryName->{'name_' . $lang} : ''; ?>
 						</div>
+						<?php if(isset($model->company_inn)) :?>
+						<div style="margin-top: 10px;">
+							<b><?= $model->getAttributeLabel('company_name') ?>:</b> <?= (isset($model->company_name)) ? $model->company_name : ''; ?>
+						</div>
+						<div style="margin-top: 10px;">
+							<b><?= $model->getAttributeLabel('company_inn') ?>:</b> <?= (isset($model->company_inn)) ? $model->company_inn : ''; ?>
+						</div>
+						<?php endif; ?>
 						<div style="margin-top: 10px;">
 							<b><?= $model->getAttributeLabel('codetnved') ?>:</b> <?= (isset($model->codetnved)) ? $model->codetnved : ''; ?>
 						</div>
@@ -82,10 +103,15 @@ $lang = Yii::$app->language;
 							<b><?= $model->getAttributeLabel('description') ?>:</b> <?= (isset($model->description)) ? $model->description : ''; ?>
 						</div>
 						<div style="margin-top: 10px;">
-							<b><?= $model->getAttributeLabel('type_of_alert') ?>:</b> <?= (isset($model->type_of_alert)) ? $model->type_of_alert : ''; ?>
+							<b><?= $model->getAttributeLabel('type_of_alert') ?>:</b><?php if (isset($model->type_of_alert))
+							  {if (is_numeric($model->type_of_alert))  echo Product::getAlert($model->type_of_alert);
+							else echo $model->type_of_alert; } ?>
 						</div>
 						<div style="margin-top: 10px;">
-							<b><?= $model->getAttributeLabel('type') ?>:</b> <?= (isset($model->type)) ? $model->type : ''; ?>
+							<b><?= $model->getAttributeLabel('type') ?>:</b>
+							 <?php if (isset($model->type))
+							  {if (is_numeric($model->type))  echo Product::getType($model->type);
+							else echo $model->type; } ?>
 						</div>
 						<div style="margin-top: 10px;">
 							<b><?= $model->getAttributeLabel('alert_number') ?>:</b> <?= (isset($model->alert_number)) ? $model->alert_number : ''; ?>
@@ -97,7 +123,9 @@ $lang = Yii::$app->language;
 							<b><?= $model->getAttributeLabel('country_of_origin') ?>:</b> <?= (isset($model->country_of_origin)) ? $model->country_of_origin : ''; ?>
 						</div>
 						<div style="margin-top: 10px;">
-							<b><?= $model->getAttributeLabel('risk_type') ?>:</b> <?= (isset($model->risk_type)) ? $model->risk_type : ''; ?>
+							<b><?= $model->getAttributeLabel('risk_type') ?>:</b> <?php if (isset($model->risk_type))
+							  {if (is_numeric($model->risk_type))  echo RiskType::find()->where(['id' =>$model->risk_type])->one()->name_cyrl;
+							else echo $model->risk_type; } ?>
 						</div>
 						<div style="margin-top: 10px;">
 							<b><?= $model->getAttributeLabel('name') ?>:</b> <?= (isset($model->name)) ? $model->name : ''; ?>
